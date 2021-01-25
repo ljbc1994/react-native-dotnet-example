9e8a819f3ec8b0e90ed0c8df431d37013ca2952a
Louie Colgan <ljbc94@gmail.com>
2021-01-25 18:48:48 +0000

    lets see

--- src/services/client/index.ts
+++ src/services/client/index.ts
@@ -2,7 +2,7 @@ import ApiClient from "./ApiClient";
 import config from "../../../config";
 
 const apiClient = new ApiClient({
-    baseUrl: config.API_URL,
+    baseUrl: process.env.API_URL,
 });
 
 export default apiClient;
